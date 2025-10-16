c1c9449bc081666420b0586cb28d6e00dbcc288a
MarkussM1327 <gpro6602@gmail.com>
2025-10-16 13:48:40 +0300

    Update uzd.py

--- dzeja/uzd.py
+++ dzeja/uzd.py
@@ -1,21 +1,19 @@
 import os
 os.system('cls')
-def birthday():
-    global x,y,cik_katram
-    try:
-        x=int(input('Ievadi cik gabalu ir kūkas - '))
-        y=int(input('Ievadi cik draugi atnākuši - '))
-        cik_katram=x/y
-        return True
-    except (ValueError,ZeroDivisionError):
-        if ValueError and not ZeroDivisionError:
-            print('Nepareiza ievade, jābūt skaitlim!')
-            return False
-        elif ZeroDivisionError:
-           print('Nepareiza ievade, ievadē nedrīkst nulle!') 
-        return False
-while True:
-    if birthday():
-        print(f'Katram pienākas {cik_katram} kūkas gabali')
-        break
-    
+dict={
+    0:7,
+    1:4,
+    2:5,
+    3:8,
+    4:1,
+    5:2,
+    6:9,
+    7:0,
+    8:3,
+    9:6
+}
+a=input("Enter a number - ").strip()
+
+if a.len()<2:
+    print(dict[a])
+
