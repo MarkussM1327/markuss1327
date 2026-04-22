5a559ad921c081d0ef46a235cc60e29bc6c8716a
MarkussM1327 <gpro6602@gmail.com>
2026-04-23 01:05:22 +0300

    Create app.py
    Signed-off-by: MarkussM1327 <gpro6602@gmail.com>

---
+++ Programma/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, redirect, url_for, request,session
+import os, json
+app=Flask(__name__)
+def data(file_name):
+    if not os.path.exists(file_name):
+        with open(file_name,'w',encoding="utf-8") as f:
+            json.dump([],f,indent=4,ensure_ascii=False)
+    with open(file_name,'r',encoding="utf-8") as f:
+        return json.load(f)
+def user_exists(username):
+    users=data("users.json")
+    for i in range(len(users)):
+        dict=users[i]
+        if dict.get("username")==username:
+            return True
+        else:
+            if i==len(users)-1:
+                return False
+app.secret_key="test"
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method=="POST":
+        if request.form.get("diff")=="ez":
+            session["game_state"]="start"
+            session["time"]=20
+        elif request.form.get("diff")=="med":
+            session["game_state"]="start"
+            session["time"]=15
+        else:
+            session["game_state"]="start"
+            session["time"]=10
+        return redirect(url_for("index"))
+    time=session.get("time")
+    if time is None:
+        time=15
+    return render_template("index.html",t=time)
+@app.route("/login",methods=["GET","POST"])
+def login():
+    error=False
+    return render_template("login.html")
+@app.route("/sign_up",methods=["GET","POST"])
+def sign_up():
+    error=False
+    if request.method=="POST":
+        user=request.form.get("username")
+        if not user_exists(user):
+            new_data={
+                "username":request.form.get("username"),
+                "password":request.form.get("password")
+            }
+            all_data=data("users.json")
+            all_data.append(new_data)
+            with open("users.json",'w',encoding="utf-8") as f:
+                json.dump(all_data,f,indent=4,ensure_ascii=False)
+                return redirect(url_for("index"))
+        else:
+            error="This username is taken"
+    return render_template("sign_up.html",error=error)
+if __name__=="__main__":
+    app.run(debug=True)
